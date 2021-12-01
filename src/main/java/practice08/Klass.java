2146eb339305db122d7f2bf6690f8fdff08afaca
Gloria Chan <changl3@oocl.com>
2021-12-01 20:34:00 +0800

    [java-oostepbystep][Gloria] -feat: Practice08 commit

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,33 @@
 package practice08;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private int klass;
+    private Student leader;
+    private final List<Student> students = new ArrayList<>();
+
+    public Klass(int klass){
+        this.klass = klass;
+    }
+
+    public int getNumber(){
+        return klass;
+    }
+
+    public String getDisplayName(){
+        return "Class " + klass;
+    }
+
+    public void assignLeader(Student student){
+        if(students.contains(student))
+            this.leader = student;
+        else
+            System.out.println("It is not one of us.");
+    }
+
+    public Student getLeader(){
+        return leader;
+    }
 }
--- src/main/java/practice08/Student.java
+++ src/main/java/practice08/Student.java
@@ -1,4 +1,32 @@
 package practice08;
 
-public class Student {
+public class Student extends Person{
+    Klass klass;
+
+    public Student(int id, String name, int age, Klass klass){
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Student(String name, int age, Klass klass){
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public int getNumber() {
+        return klass.getNumber();
+    }
+
+    public String introduce() {
+        String result = super.introduce() + String.format(" I am a Student.");
+        if(klass.getLeader() == this)
+            result += String.format(" I am Leader of Class %d.", klass.getNumber());
+        else
+            result += String.format(" I am at Class %d.", klass.getNumber());
+        return result;
+    }
 }
