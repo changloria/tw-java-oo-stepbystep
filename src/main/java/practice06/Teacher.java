91b8f82dda7674d7ff7dddf0560b5b5517fa14c1
Gloria Chan <changl3@oocl.com>
2021-12-01 19:28:59 +0800

    [java-oostepbystep][Gloria] -feat: Practice06 commit

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,34 @@
 package practice06;
 
-public class Teacher {
+import practice05.Person;
+
+public class Teacher extends Person {
+    Klass klass;
+
+    public Teacher(String name, int age, Klass klass) {
+        super (name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super (name, age);
+    }
+
+    public Klass getKlass(){
+        return klass;
+    }
+
+    public String introduce() {
+        if(klass != null)
+            return super.introduce() + String.format(" I am a Teacher. I teach %s.", klass.getDisplayName());
+        else
+            return super.introduce() + String.format(" I am a Teacher. I teach No Class.");
+    }
+
+    public String introduceWith(Student student){
+        if (student.getKlass().getNumber() == klass.getNumber())
+            return String.format("My name is %s. I am %d years old. I am a Teacher. I teach %s.", getName(), getAge(), student.getName());
+        else
+            return String.format("My name is %s. I am %d years old. I am a Teacher. I don't teach %s.", getName(), getAge(), student.getName());
+    }
 }
