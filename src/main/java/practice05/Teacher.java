8196eb7d2d452ec2577aebeeba47f8bb21bfe59e
Gloria Chan <changl3@oocl.com>
2021-12-01 18:31:31 +0800

    [java-oostepbystep][Gloria] -feat: Practice05 commit

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,23 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person {
+    private int klass;
+
+    public Teacher(String name, int age, int klass) {
+        super (name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super (name, age);
+    }
+
+    public int getKlass(){
+        return klass;
+    }
+
+
+    public String introduce() {
+        return getKlass()>0 ? super.introduce() +" "+ String.format("I am a Teacher. I teach Class %d.", klass) :  super.introduce() +" I am a Teacher. I teach No Class.";
+    }
 }
